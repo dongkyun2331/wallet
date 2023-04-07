a5e91fcce71fa1bd1a14837ee002fcde6f8f41d4
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-07 18:49:45 +0900

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import "./App.css";
 import { useState, useEffect, useCallback } from "react";
 import { ethers } from "ethers";
+
+import ConnectButton from "./components/ConnectButton";
 
 function App() {
   const [provider, setProvider] = useState(undefined);
   const [signer, setSigner] = useState(undefined);
   const [walletAddress, setWalletAddress] = useState(undefined);
-  const [currenBalance, setCurrentBalance] = useState(undefined);
+  const [currentBalance, setCurrentBalance] = useState(undefined);
   const [chainId, setChainId] = useState(undefined);
+  const [isConnected, setIsConnected] = useState(false);
 
   const connectWallet = useCallback(async () => {
     try {
-      //메타마스크 설치 된 경우
       if (typeof window.ethereum !== "undefined") {
-        //메타마스크 설치 안된 경우
+        await getMetamaskData();
+
+        setIsConnected(true);
       } else {
         alert("please install MetaMask");
       }
@@ -21,21 +26,24 @@ function App() {
     }
   }, []);
 
+  const getMetamaskData = async () => {
+    const _provider = await getProvider();
+    const _signer = await getSigner(_provider);
+    await getWalletData(_signer);
+  };
+
   const getProvider = async () => {
-    //메타마스크에서 제공하는 provider를 ethers 모듈에 저장
     const provider = await new ethers.providers.Web3Provider(window.ethereum);
-    //상태변수 저장
     setProvider(provider);
+
     return provider;
   };
 
   const getSigner = async (provider) => {
-    //메타마스크에 홈페이지 연동 승인 요청
     await provider.send("eth_requestAccounts", []);
-    //메타마스크로 서명 요청
     const signer = provider.getSigner();
-    //서명 저장
     setSigner(signer);
+
     return signer;
   };
 
@@ -45,10 +53,28 @@ function App() {
       signer.getBalance(),
       signer.getChainId(),
     ]);
-
     setWalletAddress(result[0]);
-    setCurrentBalance(Number(result[1]));
+    setCurrentBalance(Number(ethers.utils.formatEther(result[1])));
     setChainId(result[2]);
   };
+
+  return (
+    <div className="App">
+      <nav className="nav">
+        <div className="rightNav">
+          <div className="connectButtonContainer">
+            <ConnectButton
+              isConnected={isConnected}
+              connectWallet={connectWallet}
+              walletAddress={walletAddress}
+              currentBalance={currentBalance}
+              chainId={chainId}
+            />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
+
 export default App;

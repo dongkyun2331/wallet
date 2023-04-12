b417f9255552b445f8c83d9feefd8577321fe5c5
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-12 17:48:00 +0900

    a

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const ConnectButton = (props) => {
   const { isConnected, connectWallet, walletAddress } = props;
 
@@ -6,11 +8,39 @@ const ConnectButton = (props) => {
     5
   )}...${walletAddress?.substring(38)}`;
 
+  const [isNetworkOpen, setIsNetworkOpen] = useState(false); // isNetworkOpen 상태를 추가합니다.
+
+  const handleAddressClick = () => {
+    setIsNetworkOpen(true); // setIsNetworkOpen으로 isNetworkOpen 상태값 변경
+  };
+
+  const handleCloseClick = () => {
+    setIsNetworkOpen(false);
+  };
+
   return (
     <>
+      {/* isNetworkOpen이 true일 때 dimm과 network 보여주기 */}
+      {isNetworkOpen && (
+        <>
+          <div className="dimm" onClick={handleCloseClick}></div>
+          <div className="network">
+            <article className="modal-title">네트워크 변경</article>
+            <img
+              className="close"
+              onClick={handleCloseClick}
+              src="./images/ic-modal-close.svg"
+              alt=""
+            />
+          </div>
+        </>
+      )}
       {isConnected ? (
         <div className="buttonContainer">
-          <span className="pageButtonBold connectButton">
+          <span
+            className="pageButtonBold connectButton"
+            onClick={handleAddressClick}
+          >
             {displayWalletAddress}
           </span>
         </div>

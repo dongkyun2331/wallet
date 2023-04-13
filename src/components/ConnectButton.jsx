3e708a78a21339687f91b00cc4938c7c660edf8f
dongkyun2331 <119479530+dongkyun2331@users.noreply.github.com>
2023-04-13 20:08:23 +0900

    a

--- src/components/ConnectButton.jsx
+++ src/components/ConnectButton.jsx
@@ -19,6 +19,65 @@ const ConnectButton = (props) => {
     setIsNetworkOpen(false);
   };
 
+  const switchToEth = async () => {
+    try {
+      setIsNetworkOpen(false);
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: "0x1" }],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const switchToKlay = async () => {
+    try {
+      setIsNetworkOpen(false);
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x2019",
+            chainName: "Klaytn Mainnet Cypress",
+            rpcUrls: ["https://public-node-api.klaytnapi.com/v1/cypress"],
+            blockExplorerUrls: ["https://scope.klaytn.com"],
+            nativeCurrency: {
+              name: "KLAY",
+              symbol: "KLAY",
+              decimals: 18,
+            },
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const switchToGanache = async () => {
+    try {
+      setIsNetworkOpen(false);
+      await window.ethereum.request({
+        method: "wallet_addEthereumChain",
+        params: [
+          {
+            chainId: "0x539",
+            chainName: "Ganache",
+            rpcUrls: ["http://127.0.0.1:8545"],
+            nativeCurrency: {
+              name: "Ganache",
+              symbol: "ETH",
+              decimals: 18,
+            },
+          },
+        ],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       {/* isNetworkOpen이 true일 때 dimm과 network 보여주기 */}
@@ -33,6 +92,15 @@ const ConnectButton = (props) => {
               src="./images/ic-modal-close.svg"
               alt=""
             />
+            <button className="deposit" onClick={switchToEth}>
+              Ethereum mainnet
+            </button>
+            <button className="deposit" onClick={switchToKlay}>
+              Klaytn Mainnet Cypress
+            </button>
+            <button className="deposit" onClick={switchToGanache}>
+              Ganache Ethereum Testnet
+            </button>
           </div>
         </>
       )}
